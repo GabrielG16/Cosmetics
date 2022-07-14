44f0016d74ed6589902f879e118387b14e8eb436
GabrielG16 <gabriellguedes16@gmail.com>
2022-07-14 20:35:06 -0300

    Modeling

--- queries.sql
+++ queries.sql
@@ -1,19 +1,30 @@
 SELECT *
 FROM forms
 LIMIT 5;
+
+SELECT COUNT(id_form)
+FROM forms;
+
+SELECT *
+FROM forms
 LEFT JOIN order_items
 ON forms.id_form = order_items.id_form
 LIMIT 5;
 
 SELECT * 
 FROM order_items
-LIMIT 5;
+WHERE id_pedido = 'a93ced3b9bf757a'
+LIMIT 100;
 
 SELECT * 
 FROM orders
 LIMIT 5;
 
-SELECT DISTINCT f.*, o.id_pedido
+SELECT DISTINCT f.*, o.id_pedido, o.status_pagamento,
+CASE WHEN o.status_pagamento is NULL THEN 0
+	 WHEN o.status_pagamento = 'Cancelado' THEN 0
+	 ELSE 1
+	 END as target
 FROM forms f
 LEFT JOIN order_items oi
 ON f.id_form = oi.id_form

a48c230b34e39c8c8c6754009e1cfabcc1cfd1b0
GabrielG16 <gabriellguedes16@gmail.com>
2022-07-16 23:46:17 -0300

    Pipeline criacao

---
+++ data/.ipynb_checkpoints/credentials-checkpoint.py
@@ -0,0 +1,5 @@
+HOST     = 'ec2-3-222-74-92.compute-1.amazonaws.com'
+DB       = 'dfo2vqe9hs2knm'
+USR      = 'zgbodaynfjorzt'
+PORT     = '5432'
+PASS = 'd2b11c0c6039df67f39d22f680fb92a843a1b652e3d5b692f6670c0a2abc2804'
